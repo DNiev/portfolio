cd3f7cedfd6cdc2e21c81d0f42b03b1f7025e70f
DNiev <danny.nieves411@gmail.com>
2020-12-12 19:31:24 -0500

    added linked in button to new social media component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Social from './components/Social';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path ='/projects' component={Projects} />
         <Route exact path = '/contact' component={Contact} />
       </Switch>
+      <Social/>
     </BrowserRouter>
   );
 }
---
+++ src/components/Social.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Image} from 'react-bootstrap';
+
+const Social = () => {
+    return (
+        <div>
+            <a href='https://www.linkedin.com/in/danny-nieves-web-dev/'>
+                <Image className= 'socialLink' src='https://imgur.com/BoXaJfS.png' />
+            </a>
+        </div>
+    )
+};
+
+export default Social;
