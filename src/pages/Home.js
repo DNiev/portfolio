bcbbb77c6f4b87bf6a070faab3fb6fc96135a5ca
DNiev <danny.nieves411@gmail.com>
2020-12-12 22:57:29 -0500

    changed favicon.ico

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Home = () => {
     return(
         <div>
             <p>Hi, I'm Danny Nieves. I'm a web developer who loves to help small businesses, agencies, and individuals turn their ideas into living, breathing web applications. </p>
-            <p></p>
+            <p>Feel free to <Link to='/contact'>contact me</Link> if you have any questions</p>
         </div>
     )
 };
