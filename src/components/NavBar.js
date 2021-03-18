1e58df425a28a8f4fe68e21f956cd126883ea564
DNiev <danny.nieves411@gmail.com>
2021-03-18 09:23:48 -0400

    pushing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
+import {Navbar, Nav, Form, Image, Button} from 'react-bootstrap';
 
 const NavBar = () => {
     return (
         <div>
-            <Navbar bg='dark' variant='dark'>
-                <Navbar.Brand href='/'>Danny Nieves</Navbar.Brand>
+            <Navbar>
+                <Navbar.Brand href='/'><Image className='socialLink' src='https://imgur.com/ku2VU32.png'/></Navbar.Brand>
                 <Nav className='mr-auto'>
                     <Nav.Link href='/'>Home</Nav.Link>
                     <Nav.Link href='/projects'>Projects</Nav.Link>
                     <Nav.Link href='/contact'>Contact Info</Nav.Link>
                 </Nav>
                 <Form inline>
-                    <FormControl type='text' placeholder='Search' className='mr-sm-2' />
-                    <Button variant='outline-info'>Search</Button>
+                    <Button variant='outline-info' href='mailto:danny.nieves411@gmail.com'>Say Hi!</Button>
                 </Form>
             </Navbar>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,11 @@ import {Link} from 'react-router-dom';
 
 const Home = () => {
     return(
-        <div>
-            <p>Hi, I'm Danny Nieves. I'm a web developer who loves to help small businesses, agencies, and individuals turn their ideas into living, breathing web applications. </p>
-            <p>Feel free to <Link to='/contact'>contact me</Link> if you have any questions</p>
+        <div className='home'>
+            <h1 className='hero-title'>Full-Stack Web Developer</h1>
+            <h3 className='hero-desc'>Hi, I'm Danny Nieves. I'm a web developer who loves to help small businesses, agencies, and individuals turn their ideas into living, breathing web applications. </h3>
+            <h6 className='about-me'>I started my career in the Hospitality industry, and over 10 years I worked my way up the ranks to finally become an event producer. Unfortunately I hadn't planned for a global pandemic, and the Event Production industry was hit hard. Knowing an opportunity when I see one, I siezed the chance to go back to school and become a Web Developer. I'm happy to be able to apply my project management skills and attention to detail to the tech industry. Now I cater to my clients with lines of code!</h6>
+            <h6 className='contact-text'>Feel free to&ensp;<Link to='/contact'>contact me</Link>&ensp;if you have any questions!</h6>
         </div>
     )
 };
